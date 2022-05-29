43b5d9611fe3c814cacba23b4e0a584f36eb4fef
YanWei <yanwei3623918@gmail.com>
2022-05-29 21:50:18 +0800

    cart

---
+++ js/all.js
@@ -0,0 +1,12 @@
+$('document').ready(function () {
+  $('.list li a').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $(this).parent().siblings().find('a').removeClass('active');
+  });
+  // 點擊愛心時，切換效果
+  $('.menuItem i').on('click', function () {
+    $(this).toggleClass('fa-regular fa-heart');
+    $(this).toggleClass('fa-solid fa-heart');
+  });
+});
